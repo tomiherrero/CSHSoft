934f53c8d15767dad00df79007dc9a9cd2cd9c08
gonzilaime <gonzi_vte@hotmail.com>
2020-06-24 23:34:57 -0300

    Creación de estructura, instalación de paquetes, avances en front

---
+++ CSHSoft.Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CSHSoft.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
